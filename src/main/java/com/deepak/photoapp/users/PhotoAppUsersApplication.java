67db3b8de2a8adbc525e6a206ddcbd01a27f6dba
Deepak Gola <er.deepakgola@gmail.com>
2024-12-26 20:17:29 +0530

    Changes: **Introducing Encryption for password using Spring Security**
    For that purpose we used BCryptEncoder for encoding, created an instance
    of BCryptEncoder in `UsersServiceImpl` to initialize it using
    constructor.
    As BCryptPasswordEncoder is a regular class and not defined as a
    component or a bean.
    So for us to be able to inject an object of this class into user service
    implementation, we have created a bean in the form of a method in
    `PhotoAppUsersApplication` class.

--- src/main/java/com/deepak/photoapp/users/PhotoAppUsersApplication.java
+++ src/main/java/com/deepak/photoapp/users/PhotoAppUsersApplication.java
@@ -3,6 +3,8 @@ package com.deepak.photoapp.users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -11,5 +13,10 @@ public class PhotoAppUsersApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PhotoAppUsersApplication.class, args);
 	}
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
--- src/main/java/com/deepak/photoapp/users/service/impl/UsersServiceImpl.java
+++ src/main/java/com/deepak/photoapp/users/service/impl/UsersServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.deepak.photoapp.users.data.UsersRepo;
@@ -16,20 +17,22 @@ import com.deepak.photoapp.users.service.dto.UserDTO;
 public class UsersServiceImpl implements UsersService {
 	
 	UsersRepo usersRepo;
+	BCryptPasswordEncoder bCryptPasswordEncoder;
 	
 	@Autowired
-	public UsersServiceImpl(UsersRepo usersRepo) {
+	public UsersServiceImpl(UsersRepo usersRepo, BCryptPasswordEncoder bCryptPasswordEncoder) {
 		this.usersRepo = usersRepo;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 	}
 
 	@Override
 	public UserDTO createUser(UserDTO user) {
 		user.setUserId(UUID.randomUUID().toString());
+		user.setEncryptedPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 		System.out.println(user.toString());
 		ModelMapper modelMapper = new ModelMapper();
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		UserEntity userEntity = modelMapper.map(user, UserEntity.class);
-		userEntity.setEncryptedPassword("34fg37f6t48f7y459g5h489tu45tg94");
 		usersRepo.save(userEntity);
 		return user;
 	}
